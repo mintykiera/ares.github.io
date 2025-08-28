ea4f66a824dc7d03e9a90247da5336a625fef6b5
mintykiera <baileygraham2511@gmail.com>
2025-08-28 22:08:15 +0800

    ngrok

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.appendChild(confirmModal);
   }
 
-  const BASE_URL = "https://612d7b2c5190.ngrok-free.app/";
+  const BASE_URL = "https://ea40b5d76326.ngrok-free.app/";
   const API_URL = `${BASE_URL}/v1/chat/completions`;
   const LOAD_MODEL_URL = `${BASE_URL}/v1/models/load`;
 
